14d2602072c2e7a9dcd68a1e47c7fa7873e85036
FLtheLeatherman <627720734@qq.com>
2025-06-25 09:25:07 +0800

    生成 book1-image1

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,3 @@
-// for debugging
-// use console::style;
-// use image::{ImageBuffer, RgbImage};
-// use indicatif::ProgressBar;
-//
-// mod vec3;
-// mod color;
-// mod ray;
-//
-// use color::Color;
-//
-// fn main() {
-//     let path = std::path::Path::new("output/book1/image1.png");
-//     let prefix = path.parent().unwrap();
-//     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
-//
-//     let width = 256;
-//     let height = 256;
-//     // different from the book, we use image crate to create a .png image rather than outputting .ppm file, which is not widely used.
-//     // anyway, you may output any image format you like.
-//     let mut img: RgbImage = ImageBuffer::new(width, height);
-//
-//     let progress = if option_env!("CI").unwrap_or_default() == "true" {
-//         ProgressBar::hidden()
-//     } else {
-//         ProgressBar::new((height * width) as u64)
-//     };
-//
-//     for j in (0..height) {
-//         for i in 0..width {
-//             let r: f64 = (i as f64) / ((width - 1) as f64) * 255.999;
-//             let g: f64 = (j as f64) / ((height - 1) as f64) * 255.999;
-//             let b: f64 = 0.25 * 255.999;
-//             let pixel_color: Color = Color::new(r, g, b);
-//             color::write_color(i, j, &pixel_color, &mut img);
-//         }
-//         progress.inc(1);
-//     }
-//     progress.finish();
-//
-//     println!(
-//         "Output image as \"{}\"",
-//         style(path.to_str().unwrap()).yellow()
-//     );
-//     img.save(path).expect("Cannot save the image to the file");
-// }
-
 mod color;
 mod hittable;
 mod hittable_list;
@@ -78,7 +31,8 @@ fn ray_color(r: &Ray, world: &dyn Hittable) -> Color {
     Color::new(1.0, 1.0, 1.0) * (1.0 - a) + Color::new(0.5, 0.7, 1.0) * a
 }
 fn main() {
-    let aspect_ratio = 16.0 / 9.0;
+    // let aspect_ratio = 16.0 / 9.0;
+    let aspect_ratio = 1.0;
     let image_width = 400;
     let mut image_height = (image_width as f64 / aspect_ratio) as u32;
     if image_height < 1 {
@@ -104,11 +58,15 @@ fn main() {
             let pixel_center =
                 pixel100_loc + pixel_delta_u * (i as f64) + pixel_delta_v * (j as f64);
             let ray_direction = pixel_center - camera_center;
-            let r = Ray::new(camera_center, ray_direction);
-            let pixel_color = ray_color(&r, &world);
+            // let r = Ray::new(camera_center, ray_direction);
+            // let pixel_color = ray_color(&r, &world);
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0.0;
+            let pixel_color = Color::new(r, g, b);
             write_color(i, j, &pixel_color, &mut img);
         }
     }
     let path = std::path::Path::new("output/book1/image1.png");
-    img.save(path);
+    img.save(path).expect("Output Image could not be saved");
 }
