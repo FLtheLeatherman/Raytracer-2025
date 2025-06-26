878f4933f7c1742dc9b9add60d80ab546a31f695
FLtheLeatherman <627720734@qq.com>
2025-06-26 23:32:31 +0800

    feat: book1 完结

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,11 @@ mod vec3;
 use crate::camera::Camera;
 use crate::color::Color;
 use crate::material::{Dielectric, Lambertian, Metal};
+use crate::utility::{random_double, random_double_range};
 use hittable::Hittable;
 use hittable_list::HittableList;
+use material::Material;
+use rand::random;
 use sphere::Sphere;
 use std::sync::Arc;
 use utility::PI;
@@ -24,51 +27,70 @@ use vec3::Vec3;
 
 fn main() {
     let mut world: HittableList = HittableList::new();
-    let material_ground = Lambertian::new(Color::new(0.8, 0.8, 0.0));
+    let ground_material = Lambertian::new(Color::new(0.5, 0.5, 0.5));
     world.add(Box::new(Sphere::new(
-        Vec3::new(0.0, -100.5, -1.0),
-        100.0,
-        material_ground,
+        Vec3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material,
     )));
-    let material_center = Lambertian::new(Color::new(0.1, 0.2, 0.5));
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = random_double();
+            let center: Vec3 = Vec3::new(
+                a as f64 + 0.9 * random_double(),
+                0.2,
+                b as f64 + 0.9 * random_double(),
+            );
+            if (center - Vec3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    let albedo = Color::random() * Color::random();
+                    let sphere_material = Lambertian::new(albedo);
+                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                } else if choose_mat < 0.95 {
+                    let albedo = Color::random_range(0.5, 1.0);
+                    let fuzz = random_double_range(0.0, 0.5);
+                    let sphere_material = Metal::new(albedo, fuzz);
+                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                } else {
+                    let sphere_material = Dielectric::new(1.5);
+                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                }
+            }
+        }
+    }
+    let material1 = Dielectric::new(1.5);
     world.add(Box::new(Sphere::new(
-        Vec3::new(0.0, 0.0, -1.2),
-        0.5,
-        material_center,
+        Vec3::new(0.0, 1.0, 0.0),
+        1.0,
+        material1,
     )));
-    let material_left = Dielectric::new(1.50);
+    let material2 = Lambertian::new(Color::new(0.4, 0.2, 0.1));
     world.add(Box::new(Sphere::new(
-        Vec3::new(-1.0, 0.0, -1.0),
-        0.5,
-        material_left,
+        Vec3::new(-4.0, 1.0, 0.0),
+        1.0,
+        material2,
     )));
-    let material_bubble = Dielectric::new(1.00 / 1.50);
+    let material3 = Metal::new(Color::new(0.7, 0.6, 0.5), 0.0);
     world.add(Box::new(Sphere::new(
-        Vec3::new(-1.0, 0.0, -1.0),
-        0.4,
-        material_bubble,
+        Vec3::new(4.0, 1.0, 0.0),
+        1.0,
+        material3,
     )));
-    let material_right = Metal::new(Color::new(0.8, 0.6, 0.2), 1.0);
-    world.add(Box::new(Sphere::new(
-        Vec3::new(1.0, 0.0, -1.0),
-        0.5,
-        material_right,
-    )));
-    let lookfrom = Vec3::new(-2.0, 2.0, 1.0);
-    let lookat = Vec3::new(0.0, 0.0, -1.0);
+    let lookfrom = Vec3::new(13.0, 2.0, 3.0);
+    let lookat = Vec3::new(0.0, 0.0, 0.0);
     let vup = Vec3::new(0.0, 1.0, 0.0);
     let mut cam: Camera = Camera::new(
         16.0 / 9.0,
-        400,
-        100,
+        1200,
+        500,
         50,
         20.0,
         lookfrom,
         lookat,
         vup,
+        0.6,
         10.0,
-        3.4,
     );
-    let path = std::path::Path::new("output/book1/image22.png");
+    let path = std::path::Path::new("output/book1/image23.png");
     cam.render(&world, path);
 }
