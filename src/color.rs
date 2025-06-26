cbc504a44469f7e92e6eb9f3e5ca0d89aaa2b74f
FLtheLeatherman <627720734@qq.com>
2025-06-26 11:23:57 +0800

    feat: gamma correction

--- src/color.rs
+++ src/color.rs
@@ -8,10 +8,16 @@ lazy_static! {
     pub static ref INTENSITY: Interval = Interval::new(0.0, 0.999);
 }
 
+fn linear_to_gamma(linear_component: f64) -> f64 {
+    if linear_component > 0.0 {
+        return linear_component.sqrt();
+    }
+    0.0
+}
 pub fn write_color(x: u32, y: u32, pixel_color: &Color, img: &mut RgbImage) {
     let pixel = img.get_pixel_mut(x, y);
-    let r = (INTENSITY.clamp(pixel_color.x) * 256.0) as u8;
-    let g = (INTENSITY.clamp(pixel_color.y) * 256.0) as u8;
-    let b = (INTENSITY.clamp(pixel_color.z) * 256.0) as u8;
+    let r = (INTENSITY.clamp(linear_to_gamma(pixel_color.x)) * 256.0) as u8;
+    let g = (INTENSITY.clamp(linear_to_gamma(pixel_color.y)) * 256.0) as u8;
+    let b = (INTENSITY.clamp(linear_to_gamma(pixel_color.z)) * 256.0) as u8;
     *pixel = image::Rgb([r, g, b]);
 }
--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,6 @@ fn main() {
     world.add(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)));
     world.add(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)));
     let mut cam: Camera = Camera::new(16.0 / 9.0, 400, 100, 50);
-    let path = std::path::Path::new("output/book1/image11.png");
+    let path = std::path::Path::new("output/book1/image12.png");
     cam.render(&world, path);
 }
