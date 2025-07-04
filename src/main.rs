f5f4d860f509589e1d7db9c318937d395223f270
FLtheLeatherman <627720734@qq.com>
2025-07-04 16:03:35 +0800

    feat: 测试 book2 final with pdf method

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ mod sphere;
 mod texture;
 mod utility;
 mod vec3;
+mod triangle;
 
 use crate::bvh::BvhNode;
 use crate::camera::Camera;
@@ -501,123 +502,133 @@ use vec3::Vec3;
 //     cam.initialize();
 //     cam.render(&world, path);
 // }
-// fn final_scene(image_width: u32, samples_per_pixel: u32, max_depth: u32) {
-//     let mut boxes1 = HittableList::new();
-//     let ground = Arc::new(Lambertian::new(Color::new(0.48, 0.83, 0.53)));
-//     let boxes_per_size = 20;
-//     for i in 0..boxes_per_size {
-//         for j in 0..boxes_per_size {
-//             let w = 100.0;
-//             let x0 = -1000.0 + i as f64 * w;
-//             let z0 = -1000.0 + j as f64 * w;
-//             let y0 = 0.0;
-//             let x1 = x0 + w;
-//             let y1 = random_double_range(1.0, 101.0);
-//             let z1 = z0 + w;
-//             boxes1.add(make_box(
-//                 &Vec3::new(x0, y0, z0),
-//                 &Vec3::new(x1, y1, z1),
-//                 ground.clone(),
-//             ));
-//         }
-//     }
-//     let mut world = HittableList::new();
-//     world.add(Arc::new(BvhNode::new_list(&mut boxes1)));
-//     let light = Arc::new(DiffuseLight::new(&Color::new(7.0, 7.0, 7.0)));
-//     world.add(Arc::new(Quad::new(
-//         &Vec3::new(123.0, 554.0, 147.0),
-//         &Vec3::new(300.0, 0.0, 0.0),
-//         &Vec3::new(0.0, 0.0, 265.0),
-//         light,
-//     )));
-//     let center1 = Vec3::new(400.0, 400.0, 200.0);
-//     let center2 = center1 + Vec3::new(30.0, 0.0, 0.0);
-//     let sphere_material = Arc::new(Lambertian::new(Color::new(0.7, 0.3, 0.1)));
-//     world.add(Arc::new(Sphere::new_dyn(
-//         center1,
-//         center2,
-//         50.0,
-//         sphere_material,
-//     )));
-//     world.add(Arc::new(Sphere::new(
-//         Vec3::new(260.0, 150.0, 45.0),
-//         50.0,
-//         Arc::new(Dielectric::new(1.5)),
-//     )));
-//     world.add(Arc::new(Sphere::new(
-//         Vec3::new(0.0, 150.0, 145.0),
-//         50.0,
-//         Arc::new(Metal::new(Color::new(0.8, 0.8, 0.9), 1.0)),
-//     )));
-//     let boundary = Arc::new(Sphere::new(
-//         Vec3::new(360.0, 150.0, 145.0),
-//         70.0,
-//         Arc::new(Dielectric::new(1.5)),
-//     ));
-//     world.add(boundary.clone());
-//     world.add(Arc::new(ConstantMedium::new_color(
-//         boundary,
-//         0.2,
-//         &Color::new(0.2, 0.4, 0.9),
-//     )));
-//     let boundary = Arc::new(Sphere::new(
-//         Vec3::new(0.0, 0.0, 0.0),
-//         5000.0,
-//         Arc::new(Dielectric::new(1.5)),
-//     ));
-//     world.add(Arc::new(ConstantMedium::new_color(
-//         boundary,
-//         0.0001,
-//         &Color::new(1.0, 1.0, 1.0),
-//     )));
-//     let emat = Arc::new(Lambertian::new_tex(Arc::new(ImageTexture::new(
-//         "earthmap.jpg",
-//     ))));
-//     world.add(Arc::new(Sphere::new(
-//         Vec3::new(400.0, 200.0, 400.0),
-//         100.0,
-//         emat,
-//     )));
-//     let pertext = Arc::new(NoiseTexture::new(0.2));
-//     world.add(Arc::new(Sphere::new(
-//         Vec3::new(220.0, 280.0, 300.0),
-//         80.0,
-//         Arc::new(Lambertian::new_tex(pertext)),
-//     )));
-//     let mut boxes2 = HittableList::new();
-//     let white = Arc::new(Lambertian::new(Color::new(0.73, 0.73, 0.73)));
-//     let ns = 1000;
-//     for j in 0..ns {
-//         boxes2.add(Arc::new(Sphere::new(
-//             Vec3::random_range(0.0, 165.0),
-//             10.0,
-//             white.clone(),
-//         )));
-//     }
-//     world.add(Arc::new(Translate::new(
-//         Arc::new(RotateY::new(Arc::new(BvhNode::new_list(&mut boxes2)), 15.0)),
-//         Vec3::new(-100.0, 270.0, 395.0),
-//     )));
-//     let lookfrom = Vec3::new(478.0, 278.0, -600.0);
-//     let lookat = Vec3::new(278.0, 278.0, 0.0);
-//     let vup = Vec3::new(0.0, 1.0, 0.0);
-//     let mut cam: Camera = Camera::new(
-//         1.0,
-//         image_width,
-//         samples_per_pixel,
-//         max_depth as i32,
-//         40.0,
-//         lookfrom,
-//         lookat,
-//         vup,
-//         0.0,
-//         10.0,
-//         Color::new(0.0, 0.0, 0.0),
-//     );
-//     let path = std::path::Path::new("output/book2/image23.png");
-//     cam.initialize();
-//     cam.render(&world, path);
-// }
+fn final_scene(image_width: u32, samples_per_pixel: u32, max_depth: u32) {
+    let mut boxes1 = HittableList::new();
+    let ground = Arc::new(Lambertian::new(Color::new(0.48, 0.83, 0.53)));
+    let boxes_per_size = 20;
+    for i in 0..boxes_per_size {
+        for j in 0..boxes_per_size {
+            let w = 100.0;
+            let x0 = -1000.0 + i as f64 * w;
+            let z0 = -1000.0 + j as f64 * w;
+            let y0 = 0.0;
+            let x1 = x0 + w;
+            let y1 = random_double_range(1.0, 101.0);
+            let z1 = z0 + w;
+            boxes1.add(make_box(
+                &Vec3::new(x0, y0, z0),
+                &Vec3::new(x1, y1, z1),
+                ground.clone(),
+            ));
+        }
+    }
+    let mut world = HittableList::new();
+    world.add(Arc::new(BvhNode::new_list(&mut boxes1)));
+    let light = Arc::new(DiffuseLight::new(&Color::new(7.0, 7.0, 7.0)));
+    world.add(Arc::new(Quad::new(
+        &Vec3::new(123.0, 554.0, 147.0),
+        &Vec3::new(300.0, 0.0, 0.0),
+        &Vec3::new(0.0, 0.0, 265.0),
+        light,
+    )));
+    let mut lights = HittableList::new();
+    let empty_material = Arc::new(Lambertian::new(Color::new(0.0, 0.0, 0.0)));
+    lights.add(Arc::new(Quad::new(
+        &Vec3::new(123.0, 554.0, 147.0),
+        &Vec3::new(300.0, 0.0, 0.0),
+        &Vec3::new(0.0, 0.0, 265.0),
+        empty_material,
+    )));
+    let center1 = Vec3::new(400.0, 400.0, 200.0);
+    let center2 = center1 + Vec3::new(30.0, 0.0, 0.0);
+    let sphere_material = Arc::new(Lambertian::new(Color::new(0.7, 0.3, 0.1)));
+    world.add(Arc::new(Sphere::new_dyn(
+        center1,
+        center2,
+        50.0,
+        sphere_material,
+    )));
+    world.add(Arc::new(Sphere::new(
+        Vec3::new(260.0, 150.0, 45.0),
+        50.0,
+        Arc::new(Dielectric::new(1.5)),
+    )));
+    world.add(Arc::new(Sphere::new(
+        Vec3::new(0.0, 150.0, 145.0),
+        50.0,
+        Arc::new(Metal::new(Color::new(0.8, 0.8, 0.9), 1.0)),
+    )));
+    let boundary = Arc::new(Sphere::new(
+        Vec3::new(360.0, 150.0, 145.0),
+        70.0,
+        Arc::new(Dielectric::new(1.5)),
+    ));
+    world.add(boundary.clone());
+    world.add(Arc::new(ConstantMedium::new_color(
+        boundary,
+        0.2,
+        &Color::new(0.2, 0.4, 0.9),
+    )));
+    let boundary = Arc::new(Sphere::new(
+        Vec3::new(0.0, 0.0, 0.0),
+        5000.0,
+        Arc::new(Dielectric::new(1.5)),
+    ));
+    world.add(Arc::new(ConstantMedium::new_color(
+        boundary,
+        0.0001,
+        &Color::new(1.0, 1.0, 1.0),
+    )));
+    let emat = Arc::new(Lambertian::new_tex(Arc::new(ImageTexture::new(
+        "earthmap.jpg",
+    ))));
+    world.add(Arc::new(Sphere::new(
+        Vec3::new(400.0, 200.0, 400.0),
+        100.0,
+        emat,
+    )));
+    let pertext = Arc::new(NoiseTexture::new(0.2));
+    world.add(Arc::new(Sphere::new(
+        Vec3::new(220.0, 280.0, 300.0),
+        80.0,
+        Arc::new(Lambertian::new_tex(pertext)),
+    )));
+    let mut boxes2 = HittableList::new();
+    let white = Arc::new(Lambertian::new(Color::new(0.73, 0.73, 0.73)));
+    let ns = 1000;
+    for j in 0..ns {
+        boxes2.add(Arc::new(Sphere::new(
+            Vec3::random_range(0.0, 165.0),
+            10.0,
+            white.clone(),
+        )));
+    }
+    world.add(Arc::new(Translate::new(
+        Arc::new(RotateY::new(Arc::new(BvhNode::new_list(&mut boxes2)), 15.0)),
+        Vec3::new(-100.0, 270.0, 395.0),
+    )));
+    let lookfrom = Vec3::new(478.0, 278.0, -600.0);
+    let lookat = Vec3::new(278.0, 278.0, 0.0);
+    let vup = Vec3::new(0.0, 1.0, 0.0);
+    let mut cam: Camera = Camera::new(
+        1.0,
+        image_width,
+        samples_per_pixel,
+        max_depth as i32,
+        40.0,
+        lookfrom,
+        lookat,
+        vup,
+        0.0,
+        10.0,
+        Color::new(0.0, 0.0, 0.0),
+    );
+    let path = std::path::Path::new("output/book2/image23_with_pdf.png");
+    cam.initialize();
+    let world_arc: Arc<dyn Hittable> = Arc::new(world);
+    let lights_arc: Arc<dyn Hittable> = Arc::new(lights);
+    cam.render(&world_arc, &lights_arc, path);
+}
 fn book3_cornell_box() {
     let mut world = HittableList::new();
     let red = Lambertian::new(Color::new(0.65, 0.05, 0.05));
@@ -722,23 +733,15 @@ fn book3_cornell_box() {
     let lights_arc: Arc<dyn Hittable> = Arc::new(lights);
     cam.render(&world_arc, &lights_arc, path);
 }
+fn obj_test() {
+    
+}
 fn main() {
     let start = Instant::now();
-    let a = 10;
-    // match a {
-    //     1 => bouncing_spheres(),
-    //     2 => checkered_spheres(),
-    //     3 => earth(),
-    //     4 => perlin_spheres(),
-    //     5 => quads(),
-    //     6 => simple_lights(),
-    //     7 => cornell_box(),
-    //     8 => cornell_smoke(),
-    //     9 => final_scene(800, 10000, 40),
-    //     10 => book3_cornell_box(),
-    //     _ => final_scene(200, 10000, 4),
-    // }
+    let a = 9;
     match a {
+        1 => obj_test(),
+        9 => final_scene(800, 10000, 40),
         10 => book3_cornell_box(),
         _ => (),
     }
---
+++ src/triangle.rs
@@ -0,0 +1,94 @@
+use crate::aabb::AABB;
+use crate::hittable::{HitRecord, Hittable};
+use crate::hittable_list::HittableList;
+use crate::interval::Interval;
+use crate::material::Material;
+use crate::ray::Ray;
+use crate::utility::{INFINITY, random_double};
+use crate::vec3::Vec3;
+use stb_image::image::load_with_depth;
+use std::sync::Arc;
+
+pub struct Triangle {
+    q: Vec3,
+    u: Vec3,
+    v: Vec3,
+    w: Vec3,
+    mat: Arc<dyn Material>,
+    bbox: AABB,
+    normal: Vec3,
+    d: f64,
+    area: f64,
+}
+impl Triangle {
+    pub fn new(q: &Vec3, u: &Vec3, v: &Vec3, mat: Arc<dyn Material>) -> Self {
+        let bbox_diagonal1 = AABB::new_points(q, &(*q + *u + *v));
+        let bbox_diagonal2 = AABB::new_points(&(*q + *u), &(*q + *v));
+        let n = u.cross(v);
+        let normal = n.unit();
+        Self {
+            q: *q,
+            u: *u,
+            v: *v,
+            w: n / n.dot(&n),
+            mat: mat.clone(),
+            bbox: AABB::new_aabb(&bbox_diagonal1, &bbox_diagonal2),
+            normal,
+            d: normal.dot(q),
+            area: n.length(),
+        }
+    }
+    fn is_interior(a: f64, b: f64, rec: &mut HitRecord) -> bool {
+        let unit_interval = Interval::new(0.0, 1.0);
+        if !unit_interval.contains(a) || !unit_interval.contains(b) {
+            return false;
+        }
+        rec.u = a;
+        rec.v = b;
+        true
+    }
+}
+impl Hittable for Triangle {
+    fn hit(&self, r: &Ray, ray_t: &Interval, rec: &mut HitRecord) -> bool {
+        let denom = self.normal.dot(&r.direction);
+        if denom.abs() < 1e-8 {
+            return false;
+        }
+        let t = (self.d - self.normal.dot(&r.origin)) / denom;
+        if !ray_t.contains(t) {
+            return false;
+        }
+        let intersection = r.at(t);
+        let planar_hitpt_vector = intersection - self.q;
+        let alpha = self.w.dot(&planar_hitpt_vector.cross(&self.v));
+        let beta = self.w.dot(&self.u.cross(&planar_hitpt_vector));
+        if !Self::is_interior(alpha, beta, rec) {
+            return false;
+        }
+        rec.t = t;
+        rec.p = intersection;
+        rec.mat = self.mat.clone();
+        rec.set_face_normal(r, self.normal);
+        true
+    }
+    fn bounding_box(&self) -> AABB {
+        self.bbox
+    }
+    fn pdf_value(&self, origin: &Vec3, direction: &Vec3) -> f64 {
+        let mut rec = HitRecord::default();
+        if !self.hit(
+            &Ray::new(*origin, *direction),
+            &Interval::new(0.001, INFINITY),
+            &mut rec,
+        ) {
+            return 0.0;
+        }
+        let distance_squared = rec.t * rec.t * direction.squared_length();
+        let cosine = (direction.dot(&rec.normal) / direction.length()).abs();
+        distance_squared / (cosine * self.area)
+    }
+    fn random(&self, origin: &Vec3) -> Vec3 {
+        let p = self.q + (self.u * random_double()) + (self.v * random_double());
+        p - *origin
+    }
+}
